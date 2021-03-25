69e655f31a9b1829daa43741c1c0b69238562d89
DenoelaGuennoc <dguennoc@gmail.com>
2021-03-25 17:36:40 +0100

    Création de data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,34 @@
+--Thèmes
+INSERT INTO Theme VALUES('Protection');
+INSERT INTO Theme VALUES('Alerte');
+INSERT INTO Theme VALUES('Obstruction des voies aériennes');
+INSERT INTO Theme VALUES('Hémorragie');
+INSERT INTO Theme VALUES('Perte de connaissance');
+INSERT INTO Theme VALUES('Arrêt cardio-respiratoire');
+INSERT INTO Theme VALUES('Malaise');
+INSERT INTO Theme VALUES('Brûlures');
+INSERT INTO Theme VALUES('Plaies');
+INSERT INTO Theme VALUES('Traumatismes');
+
+/*
+--Chapitres
+INSERT INTO Chapitre VALUES();
+
+--Noeuds décisionnels
+INSERT INTO Chapitre VALUES();
+
+--Questions
+INSERT INTO Chapitre VALUES();
+
+--Réponses
+INSERT INTO Chapitre VALUES();
+
+--Illustrations
+INSERT INTO Chapitre VALUES();
+
+--Fiches
+INSERT INTO Chapitre VALUES();
+
+--Media
+INSERT INTO Chapitre VALUES();
+*/
