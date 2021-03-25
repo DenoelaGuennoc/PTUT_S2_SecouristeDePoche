acc06c149f9f087a0f545d8626a96335822f4a90
Escolano <escolano.gerald@gmail.com>
2021-03-25 18:08:07 +0100

    controller affiche theme semble marcher mais pas pu tester car problème bdd (Column "TITRETHEME" not found;)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,51 @@
--- Initialisation spécifiques pour un jeu de test
-INSERT INTO Theme(id, titre) VALUES (1, 'testTheme');
-INSERT INTO Chapitre(id, titre, theme) VALUES (2, 'testChapitre1', 'testTheme');
-INSERT INTO Chapitre(id, titre, theme) VALUES (3, 'testChapitre2', 'testTheme');
-INSERT INTO Chapitre(id, titre, theme) VALUES (4, 'testChapitre3', 'testTheme');
+--Thèmes
+INSERT INTO Theme(id, titreTheme) VALUES
+    (1, 'Protection'),
+    (2, 'Alerte'),
+    (3, 'Obstruction des voies aériennes'),
+    (4, 'Hémorragie'),
+    (5, 'Perte de connaissance'),
+    (6, 'Arrêt cardio-respiratoire'),
+    (7, 'Malaise'),
+    (8, 'Brûlures'),
+    (9, 'Plaies'),
+    (10, 'Traumatismes');
 
-INSERT INTO Theme(id, titre) VALUES (5, 'testTheme1000');
-INSERT INTO Chapitre(id, titre, theme) VALUES (6, 'testChapitre10000', 'testTheme1000');
-INSERT INTO Chapitre(id, titre, theme) VALUES (7, 'testChapitre20000', 'testTheme1000');
-INSERT INTO Chapitre(id, titre, theme) VALUES (8, 'testChapitre20000', 'testTheme1000');
+--Chapitres
+INSERT INTO Chapitre(id, titreChapitre, theme_id) VALUES
+    (1, "L'Obstruction partielle", 3),
+    (1, "L'Obstruction totale", 3);
+
+--Noeuds décisionnels
+INSERT INTO NouedDecisionnel VALUES
+    (1, 'Respire'),
+    (2, 'Conscient'),
+    (3, 'OBVA'),
+    (4, 'Hemo'),
+    (5, 'Trauma'),
+    (6, 'Plaie'),
+    (7, 'Brulure'),
+    (8, 'Malaise'),
+    (9, 'Type OBVA'),
+    (10, 'Type Hemo'),
+    (11, 'Localisation Trauma'),
+    (12, 'Type Plaie'),
+    (13, 'Localisation Plaie Grave'),
+    (14, 'Type Brulure'),
+    (15, 'Cause Brulure');
+/*
+--Questions
+INSERT INTO Question VALUES();
+
+--Réponses
+INSERT INTO Chapitre VALUES();
+
+--Illustrations
+INSERT INTO Chapitre VALUES();
+
+--Fiches
+INSERT INTO Chapitre VALUES();
+
+--Media
+INSERT INTO Chapitre VALUES();
+*/
