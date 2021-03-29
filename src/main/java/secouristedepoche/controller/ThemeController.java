4b419476cb674be882e0172bb2ea0de55760ffb4
DenoelaGuennoc <dguennoc@gmail.com>
2021-03-29 19:24:58 +0200

    Ajout de pages HTML

--- src/main/java/secouristedepoche/controller/ThemeController.java
+++ src/main/java/secouristedepoche/controller/ThemeController.java
@@ -2,22 +2,14 @@ package secouristedepoche.controller;
 
 import java.util.List;
 
-import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import secouristedepoche.dao.ThemeRepository;
 import secouristedepoche.entity.Theme;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.thymeleaf.spring5.SpringTemplateEngine;
-import org.thymeleaf.templateresolver.FileTemplateResolver;
 
 import lombok.extern.slf4j.Slf4j;
 
