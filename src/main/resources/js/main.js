6518623d00cb4eddb4ec54b0d2c0915650df988c
HenryCamille <camille.henry614@gmail.com>
2021-04-07 13:57:26 +0200

    commit test push

--- src/main/resources/js/main.js
+++ src/main/resources/js/main.js
@@ -1,4 +1,16 @@
 var content = document.querySelector('#hamburger-content');
-var  sidebarBody = document.querySelector('#hamburger-sidebar-body');
+var sidebarBody = document.querySelector('#hamburger-sidebar-body');
+var button = document.querySelector('#hamburger-button');
+var overlay = document.querySelector('#hamburger-overlay');
+var activatedClass = 'hamburger-activated';
 
-sidebarbody.innerHTML = content.innerHTML;
+
+sidebarBody.innerHTML = content.innerHTML;
+
+
+button.addEventListener('click', function(e){
+    e.preventDefault();
+
+    this.parentNode.classList.add(activatedClass);
+    
+})
